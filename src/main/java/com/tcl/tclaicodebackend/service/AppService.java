d8bd7d46a07f92eec2b6c7c1c9727de77f0a0765
tcl <cy.tcl@qq.com>
2026-02-25 22:05:14 +0800

    add: 应用管理CRUD

---
+++ src/main/java/com/tcl/tclaicodebackend/service/AppService.java
@@ -0,0 +1,23 @@
+package com.tcl.tclaicodebackend.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.tcl.tclaicodebackend.model.dto.app.AppQueryRequest;
+import com.tcl.tclaicodebackend.model.entity.App;
+import com.tcl.tclaicodebackend.model.vo.AppVO;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author <a href="https://github.com/tangchengliang">程序员 小南瓜</a>
+ */
+public interface AppService extends IService<App> {
+
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+}

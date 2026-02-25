524f1f213079b8fdb62bca5b6a462faead491ad9
tcl <cy.tcl@qq.com>
2026-02-25 15:08:46 +0800

    add: 用户模块

--- src/main/java/com/tcl/tclaicodebackend/TclAiCodeBackendApplication.java
+++ src/main/java/com/tcl/tclaicodebackend/TclAiCodeBackendApplication.java
@@ -1,11 +1,13 @@
 package com.tcl.tclaicodebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.tcl.tclaicodebackend.mapper")
 public class TclAiCodeBackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tcl/tclaicodebackend/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.tcl.tclaicodebackend.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.tcl.tclaicodebackend.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/tangchengliang">程序员 小南瓜</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}

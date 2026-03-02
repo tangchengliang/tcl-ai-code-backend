c3066f1a9b9ac8836868b161f5f7618f640561be
tcl <cy.tcl@qq.com>
2026-03-02 16:13:08 +0800

    add: redis-Cacheable 注解缓存精选商品数据

--- src/main/java/com/tcl/tclaicodebackend/TclAiCodeBackendApplication.java
+++ src/main/java/com/tcl/tclaicodebackend/TclAiCodeBackendApplication.java
@@ -4,11 +4,13 @@ import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStor
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class}) // 项目用不到Embedding
 @EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan("com.tcl.tclaicodebackend.mapper")
+@EnableCaching
 public class TclAiCodeBackendApplication {
 
     public static void main(String[] args) {

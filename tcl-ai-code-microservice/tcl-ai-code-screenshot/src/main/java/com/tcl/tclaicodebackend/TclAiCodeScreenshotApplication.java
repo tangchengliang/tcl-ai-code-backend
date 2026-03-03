c968d61eff2317eb19b04a33d1903f985122d634
tcl <cy.tcl@qq.com>
2026-03-03 16:31:37 +0800

    refactor: 微服务拆分-sreenshot截图服务

---
+++ tcl-ai-code-microservice/tcl-ai-code-screenshot/src/main/java/com/tcl/tclaicodebackend/TclAiCodeScreenshotApplication.java
@@ -0,0 +1,11 @@
+package com.tcl.tclaicodebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TclAiCodeScreenshotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TclAiCodeScreenshotApplication.class, args);
+    }
+}

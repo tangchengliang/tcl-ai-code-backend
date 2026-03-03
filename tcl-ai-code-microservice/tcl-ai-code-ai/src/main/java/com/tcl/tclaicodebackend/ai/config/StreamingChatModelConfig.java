e135604fd305d18b58b1f9a1d2901a8fc9ea9af9
tcl <cy.tcl@qq.com>
2026-03-03 15:46:50 +0800

    refactor: 微服务拆分-AI模块

---
+++ tcl-ai-code-microservice/tcl-ai-code-ai/src/main/java/com/tcl/tclaicodebackend/ai/config/StreamingChatModelConfig.java
@@ -0,0 +1,47 @@
+package com.tcl.tclaicodebackend.ai.config;
+
+import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@ConfigurationProperties(prefix = "langchain4j.open-ai.streaming-chat-model")
+@Data
+public class StreamingChatModelConfig {
+
+    private String baseUrl;
+
+    private String apiKey;
+
+    private String modelName;
+
+    private Integer maxTokens;
+
+    private Double temperature;
+
+    private boolean logRequests;
+
+    private boolean logResponses;
+
+    /**
+     * 流式模型
+     * @return StreamingChatModel
+     */
+    @Bean
+    @Scope("prototype")
+    public StreamingChatModel streamingChatModelPrototype() {
+        return OpenAiStreamingChatModel.builder()
+                .apiKey(apiKey)
+                .baseUrl(baseUrl)
+                .modelName(modelName)
+                .maxTokens(maxTokens)
+                .temperature(temperature)
+                .logRequests(logRequests)
+                .logResponses(logResponses)
+                .build();
+    }
+}
